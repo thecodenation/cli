bc9fd3d1c5d0836b343ea637f294021884ef375e
Code:Nation <elton.minetto@codenation.com.br>
2018-11-05 14:07:51 -0200

    Update codenation.rb

--- Formula/codenation.rb
+++ Formula/codenation.rb
@@ -1,9 +1,9 @@
 class Codenation < Formula
   desc "Code:Nation CLI"
   homepage "https://github.com/thecodenation/cli"
-  url "https://s3-us-west-1.amazonaws.com/codenation-cli/v1.0.3/codenation_1.0.3_darwin_amd64.tar.gz"
-  version "v1.0.3"
-  sha256 "930c5a5c220f698ad71cd40042745423a9769ad86146065acdaa5b5329812eb6"
+  url "https://s3-us-west-1.amazonaws.com/codenation-cli/v1.0.4/codenation_1.0.4_darwin_amd64.tar.gz"
+  version "v1.0.4"
+  sha256 "9dbaa2d96a6252ffca8478d4f3ad6986cff0d3bd99e46f2358b9a7be48ba26bb"
 
   def install
     bin.install "codenation"
